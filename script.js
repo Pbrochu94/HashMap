877b3e52f161374d79221df752b08fb39215ed6e
Patrick Brochu <patrickbrochu70@gmail.com>
2025-06-03 10:47:48 -0400

    Completed get()has()remove()length() and clear() functions
    TODO:Implement the last functions keys()values()
    entries()

--- script.js
+++ script.js
@@ -34,7 +34,44 @@ class HashMap {
     }
     if (this.load === Math.ceil(this.capacity - (this.capacity / 100) * 20)) {
       this.capacity *= 2;
+      let arrHolder = this.array;
+      this.array = [];
+      arrHolder.forEach((element) => {
+        //need to rehash every value with new capacity modulo
+        hash(element);
+      });
     }
+  }
+  get(key) {
+    if (!this.array[key]) {
+      return null;
+    }
+    return this.array[key];
+  }
+  has(key) {
+    if (!this.array[key]) {
+      return false;
+    }
+    return true;
+  }
+  remove(key) {
+    if (!this.array[key]) {
+      return false;
+    }
+    delete this.array[key];
+    this.load--;
+  }
+  length() {
+    let total = 0;
+    this.array.forEach((element) => {
+      total++;
+    });
+    return total;
+  }
+  clear() {
+    this.array = [];
+    this.load = 0;
+    this.capacity = 16;
   }
 }
 
@@ -46,8 +83,7 @@ function hash(person) {
   for (let i = 0; i < key.length; i++) {
     hashCode = primeNumber * hashCode + key.charCodeAt(i);
   }
-  hashCode %= 16; //to reduce the hashcode to fit our array of 16
-  console.log(person.lastName, hashCode);
+  hashCode %= map.capacity; //to reduce the hashcode to fit our array of 16
   map.set(hashCode, person);
 }
 
@@ -75,9 +111,13 @@ let personArr = [
   { firstName: "Mira", lastName: "Totoro" },
   { firstName: "Misao", lastName: "Atatas" },
   { firstName: "Akura", lastName: "Atame" },
+  { firstName: "Patrick", lastName: "Brochu" },
 ];
 
 for (let i = 0; i < personArr.length; i++) {
   hash(personArr[i]);
 }
+
+console.log(map);
+map.clear();
 console.log(map);
